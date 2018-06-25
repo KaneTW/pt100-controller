c0380e39ca74a7084be2ecfe7008666bca943d8f
David Kraeutmann <kane@kane.cx>
2018-06-25 02:54:45 +0200

    lifetime

--- src/main.rs
+++ src/main.rs
@@ -370,19 +370,23 @@ fn main() {
     post_reset(&mut state);
     let chs = [Channel::Ch1, Channel::Ch2, Channel::Ch3, Channel::Ch4];
 
+    let user = env::var("PT100_INFLUXDB_USER").unwrap().as_str();
+    let pass = env::var("PT100_INFLUXDB_PASS").unwrap().as_str();
+    let db = env::var("PT100_INFLUXDB_DB").unwrap().as_str();
+    let host = env::var("PT100_INFLUXDB_URL").unwrap().as_str();
+    
     let credentials = Credentials {
-        username: &env::var("PT100_INFLUXDB_USER").unwrap(),
-        password: &env::var("PT100_INFLUXDB_PASS").unwrap(),
-        database: &env::var("PT100_INFLUXDB_DB").unwrap(),
+        username: user,
+        password: pass,
+        database: db,
     };
 
-    let hosts = vec![env::var("PT100_INFLUXDB_URL").unwrap().as_str()];
-
-    let client = create_client(credentials, hosts);
+    let hosts = vec![host];
 
     let (tx,rx) = sync_channel::<(u32,f64,f64,u32,u32,i64)>(1024);
 
     thread::spawn(move || {
+        let client = create_client(credentials, hosts);
         loop {
             let (n,t,r,c,uc,ts) = rx.recv().unwrap();
             let mut measurement = Measurement::new("pt100");
